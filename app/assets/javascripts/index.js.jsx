35d03bf534dcf01c869544f7984ef01853ed09d9
Han Loong Liauw <hanloongliauw@gmail.com>
2014-07-13 15:17:43 +1000

    fixed routing

--- app/assets/javascripts/index.js.jsx
+++ app/assets/javascripts/index.js.jsx
@@ -14,41 +14,40 @@ var HomeView = React.createClass({
   getInitialState: function() {
     return {blogs: []};
   },
-  componentWillMount: function() {
-    this.loadBlogs();
-  },
-  loadBlogs: function() {
-    $.ajax({
-      url: '/blogs',
+    componentWillMount: function() {
+      this.loadBlogs();
+    },
+    loadBlogs: function() {
+      $.ajax({
+        url: '/api/blogs',
       dataType: 'json',
       type: 'GET',
       success: function(data) {
         this.setState({blogs: data});
       }.bind(this)
-    });
-  },
-  render: function() {
-    var x = this.state.blogs.map(function(d) {
-      return <li key={"blogs_" + d.id}><a href={"#/blogs/" + d.id}>{d.name}</a></li>
-    });
-    return (
-      <div>
-      <p>{this.props.message}</p>
-      <a href="#/blogs/new">New Blog</a>
-      <h3>All Blogs</h3>
-      <ul>{x}</ul>
-      </div>
-    );
-  }
+      });
+    },
+    render: function() {
+      var x = this.state.blogs.map(function(d) {
+        return <li key={"blogs_" + d.id}><a href={"/blogs/" + d.id}>{d.name}</a></li>
+      });
+      return (
+          <div>
+          <p>{this.props.message}</p>
+          <a href="/blogs/new">New Blog</a>
+          <h3>All Blogs</h3>
+          <ul>{x}</ul>
+          </div>
+          );
+    }
 });
 
 // This view is simply a form with submit the json parameters and receive the flash message
-
 var NewBlogView = React.createClass({
   handleSubmit: function() {
     var name = this.refs.name.getDOMNode().value.trim();
     $.ajax({
-      url: '/blogs',
+      url: '/api/blogs',
       dataType: 'json',
       type: 'POST',
       data: {
@@ -63,79 +62,95 @@ var NewBlogView = React.createClass({
     });
     return false;
   },
-  render: function() {
-    return (
-      <div>
-      <form onSubmit={this.handleSubmit}>
+    render: function() {
+      return (
+        <div>
+        <form onSubmit={this.handleSubmit}>
         <label>Name</label><br/>
         <input type="text" name="name" id="name" ref="name" /><br/>
         <input type="submit" value="Create Blog"/>
-      </form>
-      <a href="/">Home</a>
-      </div>
-    );
-  }
-});
-
-var Router = Backbone.Router.extend({
-  message: '',
-  routes: {
-    '': 'index',
-    "blogs/new": 'new_blog',
-    "blogs/:blog_id": 'view_blog'
-  },
-  index: function() {
-    var self = this;
-    React.renderComponent(
-      <HomeView message={self.message}/>,
-      document.getElementById('new-blog')
-    );
-  },
-  new_blog: function() {
-    React.renderComponent(
-      <NewBlogView message={self.message}/>,
-      document.getElementById('new-blog')
-    );
-  },
-  view_blog: function(blog_id) {
-    React.renderComponent(
-      <BlogView blog_id={blog_id}/>,
-      document.getElementById('new-blog')
-    )
-  }
+        </form>
+        <a href="/">Home</a>
+        </div>
+        );
+    }
 });
 
 var BlogView = React.createClass({
   getInitialState: function() {
     return {name: '', entries: []};
   },
-  componentWillMount: function() {
-    $.ajax({
-      url: '/blogs/' + this.props.blog_id,
+    componentWillMount: function() {
+      $.ajax({
+        url: '/api/blogs/' + this.props.blog_id,
       dataType: 'json',
       type: 'GET',
       success: function(data) {
         this.setState({name: data.name, entries: data.entries});
       }.bind(this)
-    });
-  },
-  render: function() {
-    var x = this.state.entries.map(function(d) {
-      if(d != null) {
-        return <li key={d.title}>
-          <p>{d.title}</p>
-          <p>{d.content}</p>
+      });
+    },
+    render: function() {
+      var x = this.state.entries.map(function(d) {
+        if(d != null) {
+          return <li key={d.title}>
+        <p>{d.title}</p>
+        <p>{d.content}</p>
         </li>
-      }
-    });
-    return (
-        <div>
-        <h1>{this.state.name}</h1>
-        <ul>{x}</ul>
-        </div>
-    );
-  }
+        }
+      });
+      return (
+          <div>
+          <h1>{this.state.name}</h1>
+          <ul>{x}</ul>
+          </div>
+          );
+    }
+});
+
+var Router = Backbone.Router.extend({
+  message: '',
+    routes: {
+      '': 'index',
+    "blogs/new": 'new_blog',
+    "blogs/:blog_id": 'view_blog'
+    },
+    index: function() {
+      var self = this;
+      React.renderComponent(
+        <HomeView message={self.message}/>,
+        document.getElementById('new-blog')
+        );
+    },
+    new_blog: function() {
+      React.renderComponent(
+        <NewBlogView message={self.message}/>,
+        document.getElementById('new-blog')
+        );
+    },
+    view_blog: function(blog_id) {
+      React.renderComponent(
+          <BlogView blog_id={blog_id}/>,
+          document.getElementById('new-blog')
+          )
+    }
 });
 
 var r = new Router();
-Backbone.history.start();
+Backbone.history.start({pushState: true});
+
+
+$(document).on("click", "a[href^='/']", function(event) {
+  var href, passThrough, url;
+  href = $(event.currentTarget).attr('href');
+  passThrough = href.indexOf('sign_out') >= 0;
+  if (!passThrough && !event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey) {
+    event.preventDefault();
+    url = href.replace(/^\//, '').replace('\#\!\/', '');
+    r.navigate(url, {
+      trigger: true
+    });
+    return false;
+  }
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'blogs/new'
-
-  get 'blogs/create'
-
   get 'home/index'
 
   root 'home#index'
-  resources :blogs do
-    resources :entries
+  scope '/api' do
+    resources :blogs do
+      resources :entries
+    end
   end
 end
